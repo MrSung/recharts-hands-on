48c6d055bddca6d6184977d1f23860a1c02d179b
Sungjoon Park <sungjoon512@gmail.com>
2021-03-30 23:25:38 +0900

    update eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
   extends: ['airbnb-typescript'],
   parserOptions: {
     project: './tsconfig.eslint.json',
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "import/prefer-default-export": "off"
   }
 };
