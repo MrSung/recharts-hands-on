544f202151934549770a2eb695568838e6b3d271
Sungjoon Park <sungjoon512@gmail.com>
2021-03-30 23:31:53 +0900

    read app component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { App } from './app';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    app
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
